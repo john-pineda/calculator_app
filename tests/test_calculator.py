75d10cd9fcfb7d13ff76974e962ba2aafb267ed6
john-pineda <jcp83@njit.edu>
2024-02-14 17:43:54 -0500

    updated file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,16 @@
-from calculator import add, subtract, multiply, divide
-
-def test_addition():
-    assert add(2, 2) == 4
-
-def test_subtraction():
-    assert subtract(2, 2) == 0
+from calculator.calculator import Calculator
 
 def test_multiplication():
-    assert multiply(3, 4) == 12
+    calculator = Calculator("multiply", 3, 4, 12)
+    assert calculator.get_calculation() == "3 multiply 4 = 12"
+    assert calculator.get_calculation() in [calc.get_calculation() for calc in Calculator.get_operation_history()]
 
 def test_division():
-    assert divide(6, 2) == 3
+    calculator = Calculator("divide", 6, 2, 3)
+    assert calculator.get_calculation() == "6 divide 2 = 3"
+    assert calculator.get_calculation() in [calc.get_calculation() for calc in Calculator.get_operation_history()]
+
+def test_new_method():
+    calculator = Calculator("new_method", 2, 3, 5)
+    assert calculator.get_calculation() == "2 new_method 3 = 5"
+    assert calculator.get_calculation() in [calc.get_calculation() for calc in Calculator.get_operation_history()]
