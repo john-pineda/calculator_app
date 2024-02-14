70df492c74ab10e80037d709016e9e9fb955aefa
john-pineda <jcp83@njit.edu>
2024-02-14 17:27:55 -0500

    adding new features for this branch will be creating a new one

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,11 +1,13 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
-def subtract(a,b):
+def subtract(a, b):
     return a - b
 
-def multiply(a,b):
+def multiply(a, b):
     return a * b
 
-def divide(a,b):
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Not divisible by 0")
     return a / b
